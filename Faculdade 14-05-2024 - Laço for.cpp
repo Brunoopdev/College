0f2109e92b5cf3dfe49ad2edc76b7a2a88152ef6
Bruno <gpaes247@gmail.com>
2024-05-14 20:54:19 -0300

    Add files via upload

---
+++ Faculdade 14-05-2024 - Laço for.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+		// 1 - Fazer um programa C que receba a nota de 2 avaliações de 10 alunos. Em seguida calcule e mostre a média para cada aluno
+	
+//	for(int i = 1; i <= 10; i++){
+//		int nota1;
+//		int nota2;
+//		
+//		printf("Coloque a primeira nota \n");
+//		scanf("%d", &nota1);
+//		printf("Coloque a segunda nota \n");
+//		scanf("%d", &nota2);
+//		
+//		int media = (nota1 + nota2) / 2;
+//		printf("A media do aluno %d e:  %d \n", i, media);
+//	}
+//	
+//	getch();
+
+		// 2 - Fazer um programa C que leia 25 números inteiros e no final mostre na tela o maior número lido
+	
+//	int maiorNumero;
+//	int numero;
+//	
+//	for(int i = 0; i < 25; i ++){
+//		
+//		printf("Escreva um numero inteiro \n");
+//		scanf("%d", &numero);
+//		
+//		if(numero > maiorNumero){
+//			maiorNumero = numero;
+//		}
+//		
+//	}
+//	
+//	printf("O maior numero e: %d", maiorNumero);
+//	getch();
+
+		// 3 - Fazer um programa C que leia um número inteiro, em seguida mostre a tabuada deste número lido usando o laço for
+		
+//		int numeroTabuada; // usuario digitou
+//		int mostraValor;
+//		
+//		printf("Voce quer a tabuada de qual numero? \n");
+//			scanf("%d", &numeroTabuada);
+//			printf("\n\n");
+//		
+//		for(int i = 1; i <= 10; i++){
+//			
+//			mostraValor = numeroTabuada * i;
+//			
+//			printf("%d X %d = %d \n",numeroTabuada, i, mostraValor);
+//		}
+}
